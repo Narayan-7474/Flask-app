923b86bd447a5144ebf591ca61c7e16795879db8
Narayan-7474 <58131708+Narayan-7474@users.noreply.github.com>
2026-03-24 21:51:07 +0530

    Update greeting message in hello_world function

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello Dosto, flask app is successfully run on jenkins'
+    return 'Hello Guys, flask app is successfully run on jenkins'
 
 @app.route('/health')
 def health():
